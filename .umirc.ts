7d66462a88d81e559f08ecf74ac89d262e1e78ff
jerrychir <jerrychir@gmail.com>
2020-07-21 23:31:25 +0800

    add electron builder

--- .umirc.ts
+++ .umirc.ts
@@ -4,5 +4,10 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  history: {
+    type: 'hash',
+  },
+  outputPath: `./dist/renderer`,
+  publicPath: './',
   routes: [{ path: '/', component: '@/renderer/pages/index' }],
 });
